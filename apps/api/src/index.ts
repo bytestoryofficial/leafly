b91d46862b7f0b1336a3927b754e80dd4bb28035
bytestoryofficial <official.byte.story@gmail.com>
2026-09-11 03:18:55 +0300

    create: temp /plants route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,22 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { Db } from 'mongodb';
+
+import getDb from '@/db/client';
+
+import Plant from '@/db/schemas/plant.schema';
 
 const app = new Hono();
 
 app.use('*', logger());
 
 app.get('/health', (context) => context.json({ status: 'OK' }));
+
+app.get('/plants', async (context) => {
+  const db: Db = await getDb();
+  const plants = await db.collection('plant_species').find().toArray();
+
+  return context.json(plants);
+});
 
 export default app;
